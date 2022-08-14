ebb21b43fea2dfe90e0fc8218680fb15cc130fb1
sato <ysudqfs@163.com>
2022-08-14 18:17:21 +0800

    fix

--- legend/legend.py
+++ legend/legend.py
@@ -1,7 +1,6 @@
 import time
 import cv2
 
-
 class Legend(object):
     fix_loc = (300, 1000)
 
@@ -13,7 +12,7 @@ class Legend(object):
         while self.parent.running and self.parent.client.alive:
             # TODO
             frame = self.parent.q.get()
-            cv2.imwrite("111.png", frame)
             self.client.device.click(*self.fix_loc)
             time.sleep(2)
         self.parent.running = False
+        self.parent.client.alive = False
